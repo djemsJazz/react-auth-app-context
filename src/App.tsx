7c37edb40cf1dd9a5e46548a6250c155846d29a1
Dahmoun Djamel <dahmdjamel92@gmail.com>
2025-06-30 16:59:10 -0400

    <refactor>: creating <MainRouter /> and moving routing specification into it

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router";
 
 import './App.css';
-import HomePage from './modules/home';
-import AboutPage from './modules/about';
-import AppLayout from './shared/components/layouts';
-import UsersRouter from './modules/users';
 import AuthReducerProvider from './contexts/AuthReducer';
-import ProtectedRoute from './shared/components/protected-route';
-import UserProfile from './modules/users/pages/UserProfile';
-import NotFoundPage from './modules/not-found/NotFoundPage';
-import ProtectedLayout from './shared/components/layouts/ProtectedLayout';
-import LoginPage from './modules/auth/pages/LoginPage';
+import MainRouter from './routing/MainRouter';
 
 const App: React.FC = () => {
   return (
     <AuthReducerProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="users/*" element={<UsersRouter />} />
-            {/* This is an example of user ProtecterRoute as a component, you must use this everytime you decalre a protected route */}
-            <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-            {/* If you have many route and you dont want to repeat ProtectedRoute, you can use the same principle, but in layout style*/}
-            <Route element={<ProtectedLayout />}>
-              <Route path="/profile-layout" element={<UserProfile />} />
-            </Route>
-            <Route path="login" element={<LoginPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <MainRouter />
     </AuthReducerProvider>
   );
 };
